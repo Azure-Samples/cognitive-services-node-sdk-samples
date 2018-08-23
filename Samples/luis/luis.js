c1437ecbb82a89ce4594a0a2f466a0e0def4cf28
Kamil Pajdzik <kamip@microsoft.com>
2018-08-23 12:51:25 -0700

    Add working app publishing

--- Samples/luis/luis.js
+++ Samples/luis/luis.js
@@ -111,6 +111,90 @@ function sample() {
     intentCreationPromise.then(intentId => {
         console.log("%s intent created with id %s", intentName, intentId)
     }).catch(errorHandler)
+
+    const batchPromise = Promise.all([ appCreationPromise, classCreationPromise, flightCreationPromise, intentCreationPromise ]).then(ids => {
+        const appId = ids[0]
+        function getExampleLabel(utterance, entityName, value) {
+            const startCharIndex = utterance.toLowerCase().indexOf(value.toLowerCase())
+    
+            return {
+                entityName: entityName,
+                startCharIndex: startCharIndex,
+                endCharIndex: startCharIndex + value.length
+            }
+        }
+
+        const utterances = [{
+            text: findEconomyToMadrid,
+            intentName: intentName,
+            entityLabels: [
+                getExampleLabel(findEconomyToMadrid, "Flight", "economy to madrid"),
+                getExampleLabel(findEconomyToMadrid, "Destination", "Madrid"),
+                getExampleLabel(findEconomyToMadrid, "Class", "economy"),
+            ]
+        }, {
+            text: findFirstToLondon,
+            intentName: intentName,
+            entityLabels: [
+                getExampleLabel(findEconomyToMadrid, "Flight", "London in first class"),
+                getExampleLabel(findEconomyToMadrid, "Destination", "London"),
+                getExampleLabel(findEconomyToMadrid, "Class", "first"),
+            ]
+        }]
+
+        return client.examples.batch(appId, versionId, utterances)
+    }).catch(errorHandler)
+    
+    batchPromise.then(_ => {
+        console.log("\nUtterances added to the %s intent", intentName)
+    }).catch(errorHandler)
+
+    const trainPromise = Promise.all([ appCreationPromise, batchPromise ]).then(ids => {
+        console.log("\nWe'll start training your app...")
+        const appId = ids[0]
+        return client.train.trainVersion(appId, versionId)
+    }).catch(errorHandler)
+
+    const isTrainedPromise = Promise.all([ appCreationPromise, trainPromise ]).then(ids => { 
+        const appId = ids[0]
+        return new Promise((resolve, reject) => {
+            let isTrained = false
+            const checkIfTrained = trainingStatus => trainingStatus.details.status == "UpToDate" || trainingStatus.details.status == "Success"
+
+            const poll = () => {
+                console.log("Polling...")
+                client.train.getStatus(appId, versionId).then(trainingStatuses => {
+                    isTrained = trainingStatuses.every(checkIfTrained) 
+                    if (isTrained) {
+                        resolve(trainingStatuses)
+                    } else {
+                        setTimeout(poll, 5000)
+                    }
+                });
+            }
+
+            poll()
+        })
+    }).catch(errorHandler)
+
+    isTrainedPromise.then(_ => console.log(_)).catch(errorHandler)
+
+    const publishPromise = Promise.all([ appCreationPromise, isTrainedPromise ]).then(ids => {
+        console.log("Your app is trained. You can now go to the LUIS portal and test it!")
+        console.log("\nWe'll start publishing your app...")
+
+        const appId = ids[0]
+        return client.apps.publish(appId, { 
+            versionId: versionId,
+            isStaging: false,
+            region: "westus"
+        })
+    }).catch(errorHandler)
+
+    publishPromise.then(publishResult => {
+        const endpoint = `${publishResult.endpointUrl}?subscription-key=${serviceKey}&q=`
+        console.log("Your app is published. You can now go to test it on\n%s", endpoint)
+    }).catch(errorHandler)
 }
 
 sample()
