dc201fa621e3b742c90c57d06d37e7151dc0a6b1
“zzhxiaofeng” <v-zihz@microsoft.com>
2022-05-19 15:12:00 +0800

    Update mgmt SDK and data SDK to track2

--- Samples/contentModerator/VideoAnalyze.js
+++ Samples/contentModerator/VideoAnalyze.js
@@ -3,20 +3,24 @@
  * Licensed under the MIT License. See License.txt in the project root for
  * license information.
  */
-'use strict';
+"use strict";
 
 const os = require("os");
-const async = require('async');
-const util = require('util');
-const uuidv4 = require('uuid/v4');
-const path = require('path');
-const url = require('url');
-const fs = require('fs');
+const async = require("async");
+const util = require("util");
+const uuidv4 = require("uuid/v4");
+const path = require("path");
+const url = require("url");
+const fs = require("fs");
 
-const MediaServices = require('azure-arm-mediaservices');
-const msRestAzure = require('ms-rest-azure');
-const msRest = require('ms-rest');
-const azureStorage = require('azure-storage');
+const { AzureMediaServices } = require("@azure/arm-mediaservices");
+const msRestAzure = require("ms-rest-azure");
+const msRest = require("ms-rest");
+const {
+  BlobServiceClient,
+  AnonymousCredential,
+} = require("@azure/storage-blob");
+const { ClientSecretCredential } = require("@azure/identity");
 
 const setTimeoutPromise = util.promisify(setTimeout);
 
@@ -27,12 +31,12 @@ const armAadAudience = "https://management.core.windows.net/";
 const aadEndpoint = "https://login.microsoftonline.com/";
 const armEndpoint = "https://management.azure.com/";
 const subscriptionId = "00000000-0000-0000-0000-000000000000";
-const accountName ="amsaccount";
-const region ="West US 2";
+const accountName = "amsaccount";
+const region = "West US 2";
 const aadClientId = "00000000-0000-0000-0000-000000000000";
 const aadSecret ="00000000-0000-0000-0000-000000000000";
 const aadTenantId ="00000000-0000-0000-0000-000000000000";
-const resourceGroup ="amsResourceGroup";
+const resourceGroup = "amsResourceGroup";
 
 // args
 const outputFolder = "outputs";
@@ -60,148 +64,276 @@ let blobName = null;
 ///////////////////////////////////////////
 
 async function sample(client) {
-  msRestAzure.loginWithServicePrincipalSecret(aadClientId, aadSecret, aadTenantId, {
-    environment: {
-      activeDirectoryResourceId: armAadAudience,
-      resourceManagerEndpointUrl: armEndpoint,
-      activeDirectoryEndpointUrl: aadEndpoint
-    }
-  }, async function(err, credentials, subscriptions) {
-    if (err) return console.log(err);
-    azureMediaServicesClient = new MediaServices(credentials, subscriptionId, armEndpoint, { noRetryPolicy: true });
-    
-    try {
-      console.log("creating video analyzer transform...");
-      let videoAnalyzerTransform = await ensureTransformExists(resourceGroup, accountName, videoAnalyzerTransformName, videoAnalyzerPreset());
-      
-      console.log("getting job input from arguments...");
-      let uniqueness = uuidv4();
-      let input = await getJobInputFromArguments(uniqueness);
-      let outputAssetName = namePrefix + '-output-' + uniqueness;
-      let jobName = namePrefix + '-job-' + uniqueness;
-      
-      console.log("creating output asset...");
-      let outputAsset = await createOutputAsset(resourceGroup, accountName, outputAssetName);
-      
-      console.log("submitting job...");
-      let job = await submitJob(resourceGroup, accountName, videoAnalyzerTransformName, jobName, input, outputAsset.name);
+  try {
+    const credentials = new ClientSecretCredential(
+      aadTenantId,
+      aadClientId,
+      aadSecret
+    );
+    azureMediaServicesClient = new AzureMediaServices(
+      credentials,
+      subscriptionId,
+      { endpoint: armEndpoint, retryOptions: { maxRetries: 0 } }
+    );
 
-      console.log("waiting for job to finish...");
-      job = await waitForJobToFinish(resourceGroup, accountName, videoAnalyzerTransformName, jobName);
-      
-      if (job.state == "Finished") {
-        await downloadResults(resourceGroup, accountName, outputAsset.name, outputFolder);
+    console.log("creating video analyzer transform...");
+    let videoAnalyzerTransform = await ensureTransformExists(
+      resourceGroup,
+      accountName,
+      videoAnalyzerTransformName,
+      videoAnalyzerPreset()
+    );
 
-        console.log("deleting jobs and assets...");
-        await azureMediaServicesClient.jobs.deleteMethod(resourceGroup, accountName, videoAnalyzerTransformName, jobName);
-        let jobInputAsset = input;
-        if (jobInputAsset && jobInputAsset.assetName) {
-          await azureMediaServicesClient.assets.deleteMethod(resourceGroup, accountName, jobInputAsset.assetName);
-        }
-        await azureMediaServicesClient.assets.deleteMethod(resourceGroup, accountName, outputAsset.name);
-      } else if (job.state == "Error") {
-        console.log(`${job.name} failed. Error details:`);
-        console.log(job.outputs[0].error);
-      } else if (job.state == "Canceled") {
-        console.log(`${job.name} was unexpectedly canceled.`);
-      } else {
-        console.log(`${job.name} is still in progress.  Current state is ${job.state}.`);
-      }    
-      console.log("done with sample");
-    } catch (err) {
-      console.log(err);
+    console.log("getting job input from arguments...");
+    let uniqueness = uuidv4();
+    let input = await getJobInputFromArguments(
+      resourceGroup,
+      accountName,
+      uniqueness
+    );
+    let outputAssetName = namePrefix + "-output-" + uniqueness;
+    let jobName = namePrefix + "-job-" + uniqueness;
+
+    console.log("creating output asset...");
+    let outputAsset = await createOutputAsset(
+      resourceGroup,
+      accountName,
+      outputAssetName
+    );
+
+    console.log("submitting job...");
+    let job = await submitJob(
+      resourceGroup,
+      accountName,
+      videoAnalyzerTransformName,
+      jobName,
+      input,
+      outputAsset.name
+    );
+
+    console.log("waiting for job to finish...");
+    job = await waitForJobToFinish(
+      resourceGroup,
+      accountName,
+      videoAnalyzerTransformName,
+      jobName
+    );
+
+    if (job.state == "Finished") {
+      await downloadResults(
+        resourceGroup,
+        accountName,
+        outputAsset.name,
+        outputFolder
+      );
+
+      console.log("deleting jobs and assets...");
+      await azureMediaServicesClient.jobs.delete(
+        resourceGroup,
+        accountName,
+        videoAnalyzerTransformName,
+        jobName
+      );
+      let jobInputAsset = input;
+      if (jobInputAsset && jobInputAsset.assetName) {
+        await azureMediaServicesClient.assets.delete(
+          resourceGroup,
+          accountName,
+          jobInputAsset.assetName
+        );
+      }
+      await azureMediaServicesClient.assets.delete(
+        resourceGroup,
+        accountName,
+        outputAsset.name
+      );
+    } else if (job.state == "Error") {
+      console.log(`${job.name} failed. Error details:`);
+      console.log(job.outputs[0].error);
+    } else if (job.state == "Canceled") {
+      console.log(`${job.name} was unexpectedly canceled.`);
+    } else {
+      console.log(
+        `${job.name} is still in progress.  Current state is ${job.state}.`
+      );
     }
-  });
+    console.log("done with sample");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function videoAnalyzerPreset() {
   return {
     audioLanguage: null,
-    odatatype: "#Microsoft.Media.VideoAnalyzerPreset"
-  }
+    odataType: "#Microsoft.Media.VideoAnalyzerPreset",
+  };
 }
 
-async function ensureTransformExists(resourceGroup, accountName, transformName, preset) {
-  let transform = await azureMediaServicesClient.transforms.get(resourceGroup, accountName, transformName);
-  if (!transform) {
-    transform = await azureMediaServicesClient.transforms.createOrUpdate(resourceGroup, accountName, transformName, {
+async function ensureTransformExists(
+  resourceGroup,
+  accountName,
+  transformName,
+  preset
+) {
+  await azureMediaServicesClient.transforms.createOrUpdate(
+    resourceGroup,
+    accountName,
+    transformName,
+    {
       name: transformName,
       location: region,
-      outputs: [{
-        preset: preset
-      }]
-    });
-  }
+      outputs: [
+        {
+          preset: preset,
+        },
+      ],
+    }
+  );
+  const transform = await azureMediaServicesClient.transforms.get(
+    resourceGroup,
+    accountName,
+    transformName
+  );
   return transform;
 }
 
-async function getJobInputFromArguments(resourceGroup, accountName, uniqueness) {
+async function getJobInputFromArguments(
+  resourceGroup,
+  accountName,
+  uniqueness
+) {
   if (inputFile) {
     let assetName = namePrefix + "-input-" + uniqueness;
     await createInputAsset(resourceGroup, accountName, assetName, inputFile);
     return {
-      odatatype: "#Microsoft.Media.JobInputAsset",
-      assetName: assetName
-    }
+      odataType: "#Microsoft.Media.JobInputAsset",
+      assetName: assetName,
+    };
   } else {
     return {
-      odatatype: "#Microsoft.Media.JobInputHttp",
-      files: [inputUrl]
-    }
+      odataType: "#Microsoft.Media.JobInputHttp",
+      files: [inputUrl],
+    };
   }
 }
 
-async function createInputAsset(resourceGroup, accountName, assetName, fileToUpload) {
-  let asset = await azureMediaServicesClient.assets.createOrUpdate(resourceGroup, accountName, assetName, {});
+async function createInputAsset(
+  resourceGroup,
+  accountName,
+  assetName,
+  fileToUpload
+) {
+  let asset = await azureMediaServicesClient.assets.createOrUpdate(
+    resourceGroup,
+    accountName,
+    assetName,
+    {}
+  );
   let date = new Date();
   date.setHours(date.getHours() + 1);
   let input = {
     permissions: "ReadWrite",
-    expiryTime: date
-  }
-  let response = await azureMediaServicesClient.assets.listContainerSas(resourceGroup, accountName, assetName, input);
+    expiryTime: date,
+  };
+  let response = await azureMediaServicesClient.assets.listContainerSas(
+    resourceGroup,
+    accountName,
+    assetName,
+    input
+  );
   let uploadSasUrl = response.assetContainerSasUrls[0] || null;
   let fileName = path.basename(fileToUpload);
   let sasUri = url.parse(uploadSasUrl);
-  let sharedBlobService = azureStorage.createBlobServiceWithSas(sasUri.host, sasUri.search);
-  let containerName = sasUri.pathname.replace(/^\/+/g, '');
+  let sharedBlobService = new BlobServiceClient(
+    `https://${sasUri.host}${sasUri.search}`,
+    new AnonymousCredential()
+  );
+  let containerName = sasUri.pathname.replace(/^\/+/g, "");
   let randomInt = Math.round(Math.random() * 100);
   blobName = fileName + randomInt;
   console.log("uploading to blob...");
-  function createBlobPromise() {
-    return new Promise(function (resolve, reject) {
-      sharedBlobService.createBlockBlobFromLocalFile(containerName, blobName, fileToUpload, resolve);
-    });
+  async function createBlobPromise() {
+    let containerClient = sharedBlobService.getContainerClient(containerName);
+    let blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    return await blockBlobClient.uploadFile(fileToUpload);
   }
   await createBlobPromise();
-  return asset;
+  return await azureMediaServicesClient.assets.get(
+    resourceGroup,
+    accountName,
+    assetName
+  );
 }
 
 async function createOutputAsset(resourceGroup, accountName, assetName) {
-  return await azureMediaServicesClient.assets.createOrUpdate(resourceGroup, accountName, assetName, {});
+  await azureMediaServicesClient.assets.createOrUpdate(
+    resourceGroup,
+    accountName,
+    assetName,
+    {}
+  );
+  return await azureMediaServicesClient.assets.get(
+    resourceGroup,
+    accountName,
+    assetName
+  );
 }
 
-async function submitJob(resourceGroup, accountName, transformName, jobName, jobInput, outputAssetName) {
+async function submitJob(
+  resourceGroup,
+  accountName,
+  transformName,
+  jobName,
+  jobInput,
+  outputAssetName
+) {
   let jobOutputs = [
     {
-      odatatype: "#Microsoft.Media.JobOutputAsset",
-      assetName: outputAssetName
-    }
+      odataType: "#Microsoft.Media.JobOutputAsset",
+      assetName: outputAssetName,
+    },
   ];
-  return await azureMediaServicesClient.jobs.create(resourceGroup, accountName, transformName, jobName, {
-    input: jobInput,
-    outputs: jobOutputs
-  });
+  await azureMediaServicesClient.jobs.create(
+    resourceGroup,
+    accountName,
+    transformName,
+    jobName,
+    {
+      input: jobInput,
+      outputs: jobOutputs,
+    }
+  );
+  return await azureMediaServicesClient.jobs.get(
+    resourceGroup,
+    accountName,
+    transformName,
+    jobName
+  );
 }
 
-async function waitForJobToFinish(resourceGroup, accountName, transformName, jobName) {
+async function waitForJobToFinish(
+  resourceGroup,
+  accountName,
+  transformName,
+  jobName
+) {
   let timeout = new Date();
   timeout.setSeconds(timeout.getSeconds() + timeoutSeconds);
 
   async function pollForJobStatus() {
-    let job = await azureMediaServicesClient.jobs.get(resourceGroup, accountName, transformName, jobName);
+    let job = await azureMediaServicesClient.jobs.get(
+      resourceGroup,
+      accountName,
+      transformName,
+      jobName
+    );
     console.log(job.state);
-    if (job.state == 'Finished' || job.state == 'Error' || job.state == 'Canceled') {
+    if (
+      job.state == "Finished" ||
+      job.state == "Error" ||
+      job.state == "Canceled"
+    ) {
       return job;
     } else if (new Date() > timeout) {
       console.log(`Job ${job.name} timed out.`);
@@ -214,7 +346,12 @@ async function waitForJobToFinish(resourceGroup, accountName, transformName, job
   return await pollForJobStatus();
 }
 
-async function downloadResults(resourceGroup, accountName, assetName, resultsFolder) {
+async function downloadResults(
+  resourceGroup,
+  accountName,
+  assetName,
+  resultsFolder
+) {
   try {
     fs.mkdirSync(resultsFolder);
   } catch (err) {
@@ -225,14 +362,23 @@ async function downloadResults(resourceGroup, accountName, assetName, resultsFol
   date.setHours(date.getHours() + 1);
   let input = {
     permissions: "Read",
-    expiryTime: date
-  }
-  let assetContainerSas = await azureMediaServicesClient.assets.listContainerSas(resourceGroup, accountName, assetName, input);
+    expiryTime: date,
+  };
+  let assetContainerSas =
+    await azureMediaServicesClient.assets.listContainerSas(
+      resourceGroup,
+      accountName,
+      assetName,
+      input
+    );
 
   let containerSasUrl = assetContainerSas.assetContainerSasUrls[0] || null;
   let sasUri = url.parse(containerSasUrl);
-  let sharedBlobService = azureStorage.createBlobServiceWithSas(sasUri.host, sasUri.search);
-  let containerName = sasUri.pathname.replace(/^\/+/g, '');
+  let sharedBlobService = new BlobServiceClient(
+    `https://${sasUri.host}${sasUri.search}`,
+    new AnonymousCredential()
+  );
+  let containerName = sasUri.pathname.replace(/^\/+/g, "");
   let directory = path.join(resultsFolder, assetName);
   try {
     fs.mkdirSync(directory);
@@ -240,23 +386,17 @@ async function downloadResults(resourceGroup, accountName, assetName, resultsFol
     // directory exists
   }
   console.log(`gathering blobs in container ${containerName}...`);
-  function createBlobListPromise() {
-    return new Promise(function (resolve, reject) {
-      return sharedBlobService.listBlobsSegmented(containerName, null, (err, result, response) => {
-        if (err) { reject(err); }
-        resolve(result);
-      });
-    });
+  const containerClient = sharedBlobService.getContainerClient(containerName);
+  async function createBlobListPromise() {
+    return containerClient.listBlobsFlat();
   }
   let blobs = await createBlobListPromise();
   console.log("downloading blobs to local directory in background...");
-  for (let i = 0; i < blobs.entries.length; i++){
-    let blob = blobs.entries[i];
-    if (blob.blobType == "BlockBlob"){
+  for await (const blob of blobs) {
+    if (blob.properties.blobType == "BlockBlob") {
       console.log(blob.name);
-      sharedBlobService.getBlobToLocalFile(containerName, blob.name, path.join(directory, blob.name), (error, result) => {
-        if (error) console.log(error);
-      });
+      const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
+      await blockBlobClient.downloadToFile(path.join(directory, blob.name));
     }
   }
 }
